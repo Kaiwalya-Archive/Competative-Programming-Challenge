dc4daab6ed019599e13abfe85f13f557751748a8
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-19 10:49:18 +0530

    Day 2 Challenge completed

--- TheWireUsChallenge2020/src/Day2Challenge/Solution.java
+++ TheWireUsChallenge2020/src/Day2Challenge/Solution.java
@@ -1,5 +1,24 @@
+/*
+ * 
+ *@author : Kaiwalya Koparkar
+ *@date : 19 /9 /2020
+ *@resource : https://thewireuschallenge.com  
+ *@question : Seperate the camel cased sentence 
+ *
+ */
 package Day2Challenge;
+import java.util.*;
 
 public class Solution {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		sc.close();
+		for (String w : s.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")) {
+	        System.out.println(w);
+	    }
+		
+	}
 }
