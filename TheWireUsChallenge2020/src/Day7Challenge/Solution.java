9ab88aa78973d2f3fdaafb45b83d5aa6fd7be04e
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-09-24 12:58:31 +0530

    Update Solution.java

--- TheWireUsChallenge2020/src/Day7Challenge/Solution.java
+++ TheWireUsChallenge2020/src/Day7Challenge/Solution.java
@@ -1,5 +1,54 @@
+
+/*
+ * 
+ *@author : Kaiwalya Koparkar
+ *@date : 24 /9 /2020
+ *@resource : https://thewireuschallenge.com  
+ *@question :Write a program which can calculate the occurance of the characters in the string
+ *
+ */
 package Day7Challenge;
+import java.util.*;
+class Solution{ 
+	static final int MAX_CHAR = 256; 
 
-public class Solution {
+	static void getOccuringChar(String str) 
+	{ 
+		// Create an array of size 256 i.e. ASCII_SIZE 
+		int count[] = new int[MAX_CHAR]; 
 
-}
+		int len = str.length(); 
+
+		// Initialize count array index 
+		for (int i = 0; i < len; i++) 
+			count[str.charAt(i)]++; 
+
+		// Create an array of given String size 
+		char ch[] = new char[str.length()]; 
+		for (int i = 0; i < len; i++) { 
+			ch[i] = str.charAt(i); 
+			int find = 0; 
+			for (int j = 0; j <= i; j++) { 
+
+				// If any matches found 
+				if (str.charAt(i) == ch[j]) 
+					find++;				 
+			} 
+
+			if (find == 1){
+              	System.out.print( str.charAt(i));
+                if(count[str.charAt(i)] != 1){
+            	System.out.print(count[str.charAt(i)]);
+                }
+            } 
+				
+          		
+		} 
+	} 
+	public static void main(String[] args) 
+	{ 
+		Scanner sc = new Scanner(System.in); 
+		String str = sc.nextLine(); 
+		getOccuringChar(str); 
+	} 
+} 
